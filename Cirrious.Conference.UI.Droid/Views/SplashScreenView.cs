cf222f794fe01108defaa2bac1b1b3ac3aef8361
keni_000 <kenibarwick@gmail.com>
2014-07-17 14:44:26 +0100

    Changed screen name to CIAF

--- Cirrious.Conference.UI.Droid/Views/SplashScreenView.cs
+++ Cirrious.Conference.UI.Droid/Views/SplashScreenView.cs
@@ -13,7 +13,7 @@ using Cirrious.Conference.Core.ViewModels;
 
 namespace Cirrious.Conference.UI.Droid.Views
 {
-    [Activity(Label = "SqlBits", NoHistory = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Chilled in a Field", NoHistory = true, Icon = "@drawable/icon")]
     public class SplashScreenView : BaseView<SplashScreenViewModel>
     {
         public class SplashCountDownTimer : CountDownTimer
