213b65d5fe158e3b6ae5d8b5b280a87c6222b08e
keni_000 <kenibarwick@gmail.com>
2014-07-20 12:37:42 +0100

    Added day number to help with ordering of acts when they start after midnight

--- Cirrious.Conference.Core/Models/Raw/Session.cs
+++ Cirrious.Conference.Core/Models/Raw/Session.cs
@@ -5,13 +5,14 @@ namespace Cirrious.Conference.Core.Models.Raw
     public class Session
     {
         public string Key { get; set; }
+        public int Day { get; set; }
         public DateTime When { get; set; }
+        public DateTime Until { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string SpeakerKey { get; set; }
         public string Type { get; set; }
         public string Level { get; set; }
         public string Where { get; set; }
-        public DateTime Until { get; set; }
     }
 }
