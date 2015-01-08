ee89482a7adf52646b79d250f90297dd03c0ea9c
tom <unknown-user@192.168.0.15>
2015-01-08 22:56:10 +0000

    * ciaf.userprefs: * Setup.cs: * App.xaml: * App.xaml.cs: * DebugTrace.cs: * packages.config: * Logo.png: * ReadMe.txt: * BaseView.cs: * HomeView.xaml: * SmallLogo.png: * Package.appxmanifest: * StoreLogo.png: * BindableBase.cs: * HomeView.xaml.cs: * SplashScreen.png: * RichTextColumns.cs: * LayoutAwarePage.cs: * AssemblyInfo.cs: * StandardStyles.xaml: * SuspensionManager.cs: * Cirrious.Conference.UI.Droid.csproj: * WindowsStore_TemporaryKey.pfx: * BooleanNegationConverter.cs: * MessengerPluginBootstrap.cs: * BooleanToVisibilityConverter.cs: * Cirrious.Conference.UI.WindowsStore.csproj: * ResourceLoaderPluginBootstrap.cs: * JsonLocalisationPluginBootstrap.cs:
    * ciaf.sln: remove windows store app

--- Cirrious.Conference.UI.WindowsStore/Bootstrap/JsonLocalisationPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace Cirrious.Conference.UI.WindowsStore.Bootstrap
-{
-    public class JsonLocalisationPluginBootstrap
-        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.JsonLocalisation.PluginLoader>
-    {
-    }
-}
