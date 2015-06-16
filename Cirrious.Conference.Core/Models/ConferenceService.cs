184ba7a72dfa99789b43cbd65263f34ef2c68fd9
kenibarwick <unknown-user@192.168.1.15>
2015-06-17 00:12:41 +0100

    plumb Session data to sessions

--- Cirrious.Conference.Core/Models/ConferenceService.cs
+++ Cirrious.Conference.Core/Models/ConferenceService.cs
@@ -17,143 +17,138 @@ using System.Threading;
 
 namespace Cirrious.Conference.Core.Models
 {
-    public class ConferenceService 
+	public class ConferenceService 
         : IConferenceService
-        
-    {
-        private readonly FavoritesSaver _favoritesSaver = new FavoritesSaver();
+	{
+		private readonly FavoritesSaver _favoritesSaver = new FavoritesSaver ();
 
-        // is loading setup
-        private bool _isLoading;
-        public bool IsLoading
-        {
-            get { return _isLoading; }
-            private set 
-            { 
-                _isLoading = value;
-                FireLoadingChanged();
-            }
-        }
+		// is loading setup
+		private bool _isLoading;
 
-        private void FireLoadingChanged()
-        {
-			FireMessage(new LoadingChangedMessage(this));
-        }
- 
-        // the basic lists
-        public IDictionary<string, SessionWithFavoriteFlag> Sessions { get; private set; }
-        public IDictionary<string, Sponsor> Exhibitors { get; private set; }
-        public IDictionary<string, Sponsor> Sponsors { get; private set; }
-
-        // a hashtable of favorites
-        private IDictionary<string, SessionWithFavoriteFlag> _favoriteSessions;
-        public IDictionary<string,SessionWithFavoriteFlag> GetCopyOfFavoriteSessions()
-        {
-            lock (this)
-            {
-				if (_favoriteSessions == null)
-					return new Dictionary<string, SessionWithFavoriteFlag>();
-				
-                var toReturn = new Dictionary<string, SessionWithFavoriteFlag>(_favoriteSessions);
-                return toReturn;
-            }
-        }
-		 
-        private void FireFavoriteSessionsChanged()
-        {
-			FireMessage(new FavoritesChangedMessage(this));
-        }
-
-		private void FireMessage(MvxMessage message)
-		{
-			var messenger = Mvx.Resolve<IMvxMessenger>();
-			messenger.Publish(message);
+		public bool IsLoading {
+			get { return _isLoading; }
+			private set { 
+				_isLoading = value;
+				FireLoadingChanged ();
+			}
 		}
 
-        public void BeginAsyncLoad()
-        {
-            IsLoading = true;
-            MvxAsyncDispatcher.BeginAsync(Load);
-        }
-		
-		public void DoSyncLoad()
-        {
-            IsLoading = true;
-            Load();
-        }
+		private void FireLoadingChanged ()
+		{
+			FireMessage (new LoadingChangedMessage (this));
+		}
+ 
+		// the basic lists
+		public IDictionary<string, SessionWithFavoriteFlag> Sessions { get; private set; }
 
-        private void Load()
-        {
-            LoadSessions2();
-            LoadFavorites();
-            LoadSponsors();
+		public IDictionary<string, Sponsor> Exhibitors { get; private set; }
 
-            IsLoading = false;
-        }		
-		
-        private void LoadSponsors()
-        {
-            var file = Mvx.Resolve<IMvxResourceLoader>().GetTextResource("ConfResources/Sponsors.txt");
-            var jsonConvert = Mvx.Resolve<IMvxJsonConverter>();
-            var items = jsonConvert.DeserializeObject<List<Sponsor>>(file);
-            Sponsors = items.Where(x => x.Level != "Exhibitor").ToDictionary(x => x.Name);
-            Exhibitors = items.Where(x => x.Level == "Exhibitor").ToDictionary(x => x.Name);
-        }
+		public IDictionary<string, Sponsor> Sponsors { get; private set; }
 
-        private void LoadFavorites()
-        {
-            lock (this)
-            {
-                _favoriteSessions = new Dictionary<string, SessionWithFavoriteFlag>();
-            }
-            FireFavoriteSessionsChanged();
+		// a hashtable of favorites
+		private IDictionary<string, SessionWithFavoriteFlag> _favoriteSessions;
 
-            var files = Mvx.Resolve<IMvxFileStore>();
-            string json;
-            if (!files.TryReadTextFile(Constants.FavoritesFileName, out json))
-                return;
+		public IDictionary<string,SessionWithFavoriteFlag> GetCopyOfFavoriteSessions ()
+		{
+			lock (this) {
+				if (_favoriteSessions == null)
+					return new Dictionary<string, SessionWithFavoriteFlag> ();
+				
+				var toReturn = new Dictionary<string, SessionWithFavoriteFlag> (_favoriteSessions);
+				return toReturn;
+			}
+		}
 
-            var jsonConvert = Mvx.Resolve<IMvxJsonConverter>();
-            var parsedKeys = jsonConvert.DeserializeObject<List<string>>(json);
-            if (parsedKeys != null)
-            {
-                foreach (var key in parsedKeys)
-                {
-                    SessionWithFavoriteFlag session;
-                    if (Sessions.TryGetValue(key, out session))
-                        session.IsFavorite = true;
-                }
-            }
-        }
+		private void FireFavoriteSessionsChanged ()
+		{
+			FireMessage (new FavoritesChangedMessage (this));
+		}
 
-        private void LoadSessions()
-        {
-            var file = Mvx.Resolve<IMvxResourceLoader>().GetTextResource("ConfResources/Sessions.txt");
-            var jsonConvert = Mvx.Resolve<IMvxJsonConverter>();
-            var items = jsonConvert.DeserializeObject<List<Session>>(file);
-            //foreach (var item in items)
-            //{
-            //    item.Key = item.Key;
-            //}
-            Sessions = items.Select(x => new SessionWithFavoriteFlag()
-                                                  {
-                                                      Session = x,
-                                                      IsFavorite = false
-                                                  })
-                .ToDictionary(x => x.Session.Key, x => x);
+		private void FireMessage (MvxMessage message)
+		{
+			var messenger = Mvx.Resolve<IMvxMessenger> ();
+			messenger.Publish (message);
+		}
 
-            foreach (var sessionWithFavoriteFlag in Sessions.Values)
-            {
-                sessionWithFavoriteFlag.PropertyChanged += SessionWithFavoriteFlagOnPropertyChanged;            
-            }
-        }
+		public void BeginAsyncLoad ()
+		{
+			IsLoading = true;
+			MvxAsyncDispatcher.BeginAsync (Load);
+		}
 
-		private async void LoadSessions2()
+		public void DoSyncLoad ()
+		{
+			IsLoading = true;
+			Load ();
+		}
+
+		private void Load ()
+		{
+			LoadSessions2 ();
+			LoadFavorites ();
+			LoadSponsors ();
+
+			IsLoading = false;
+		}
+
+		private void LoadSponsors ()
+		{
+			var file = Mvx.Resolve<IMvxResourceLoader> ().GetTextResource ("ConfResources/Sponsors.txt");
+			var jsonConvert = Mvx.Resolve<IMvxJsonConverter> ();
+			var items = jsonConvert.DeserializeObject<List<Sponsor>> (file);
+			Sponsors = items.Where (x => x.Level != "Exhibitor").ToDictionary (x => x.Name);
+			Exhibitors = items.Where (x => x.Level == "Exhibitor").ToDictionary (x => x.Name);
+		}
+
+		private void LoadFavorites ()
+		{
+			lock (this) {
+				_favoriteSessions = new Dictionary<string, SessionWithFavoriteFlag> ();
+			}
+			FireFavoriteSessionsChanged ();
+
+			var files = Mvx.Resolve<IMvxFileStore> ();
+			string json;
+			if (!files.TryReadTextFile (Constants.FavoritesFileName, out json))
+				return;
+
+			var jsonConvert = Mvx.Resolve<IMvxJsonConverter> ();
+			var parsedKeys = jsonConvert.DeserializeObject<List<string>> (json);
+			if (parsedKeys != null) {
+				foreach (var key in parsedKeys) {
+					SessionWithFavoriteFlag session;
+					if (Sessions.TryGetValue (key, out session))
+						session.IsFavorite = true;
+				}
+			}
+		}
+
+		private void LoadSessions ()
+		{
+			var file = Mvx.Resolve<IMvxResourceLoader> ().GetTextResource ("ConfResources/Sessions.txt");
+			var jsonConvert = Mvx.Resolve<IMvxJsonConverter> ();
+			var items = jsonConvert.DeserializeObject<List<Session>> (file);
+			//foreach (var item in items)
+			//{
+			//    item.Key = item.Key;
+			//}
+			Sessions = items.Select (x => new SessionWithFavoriteFlag () {
+				Session = x,
+				IsFavorite = false
+			})
+                .ToDictionary (x => x.Session.Key, x => x);
+
+			foreach (var sessionWithFavoriteFlag in Sessions.Values) {
+				sessionWithFavoriteFlag.PropertyChanged += SessionWithFavoriteFlagOnPropertyChanged;            
+			}
+		}
+
+		private async void LoadSessions2 ()
 		{
 			string contents;
 
-			var files = Mvx.Resolve<IMvxFileStore>();
-			files.TryReadTextFile("RESTDATA.json", out contents);
+			var files = Mvx.Resolve<IMvxFileStore> ();
+			files.TryReadTextFile ("RESTDATA.json", out contents);
 
 			var loaded = false;
 
@@ -187,78 +182,109 @@ namespace Cirrious.Conference.Core.Models
 			//LoadSessionsFromPocketConferenceModel(conferenceModel);
 		}
 
-        private void SessionWithFavoriteFlagOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
-        {
-            if (propertyChangedEventArgs.PropertyName != "IsFavorite")
-                return;
-
-            var session = (SessionWithFavoriteFlag)sender;
-            lock (this)
-            {
-                if (_favoriteSessions == null)
-                    return;
-
-                if (session.IsFavorite)
-                {
-                    _favoriteSessions[session.Session.Key] = session;
-                }
-                else
-                {
-                    if (_favoriteSessions.ContainsKey(session.Session.Key))
-                        _favoriteSessions.Remove(session.Session.Key);
-                }
-
-                _favoritesSaver.RequestAsyncSave(_favoriteSessions.Keys.ToList());
-            }
-
-            FireFavoriteSessionsChanged();
-        }
-
-		private void ParseSessions(SessionData[] data)
+		private void SessionWithFavoriteFlagOnPropertyChanged (object sender, PropertyChangedEventArgs propertyChangedEventArgs)
 		{
-			Sessions = new Dictionary<string, SessionWithFavoriteFlag>();
+			if (propertyChangedEventArgs.PropertyName != "IsFavorite")
+				return;
+
+			var session = (SessionWithFavoriteFlag)sender;
+			lock (this) {
+				if (_favoriteSessions == null)
+					return;
+
+				if (session.IsFavorite) {
+					_favoriteSessions [session.Session.Key] = session;
+				} else {
+					if (_favoriteSessions.ContainsKey (session.Session.Key))
+						_favoriteSessions.Remove (session.Session.Key);
+				}
+
+				_favoritesSaver.RequestAsyncSave (_favoriteSessions.Keys.ToList ());
+			}
+
+			FireFavoriteSessionsChanged ();
+		}
+
+		public string DayFromInt (int numberDay)
+		{
+			string day;
+			switch (numberDay) {
+			case 1:
+				day = "Friday";
+				break;
+			case 2:
+				day = "Saturday";
+				break;
+			case 3:
+			default:
+				day = "Sunday";
+				break;
+			}
+			return day;
+		}
+
+		public int DayToInt (string dayName)
+		{
+			int day;
+			switch (dayName) {
+			case "Friday":
+				day = 1;
+				break;
+			case "Saturday":
+				day = 2;
+				break;
+			case "Sunday":
+			default:
+				day = 3;
+				break;
+			}
+			return day;
+		}
+
+		private DateTime MakeDateTime(string day, string time)
+		{
+			DateTime d = new DateTime (DateTime.Now.Year, 1, 1)
+				.AddDays (210 + DayToInt (day));
+
+			TimeSpan ts = TimeSpan.Parse (time); //06:12:00
+
+			return d.Add(ts);
+		}
+
+		private void ParseSessions (SessionData[] data)
+		{
+			Sessions = new Dictionary<string, SessionWithFavoriteFlag> ();
 			var i = 0;
-			foreach (var sessionData in data)
-			{
-				var session = new Session
-				{
+			foreach (var sessionData in data) {
+				var session = new Session {
 
-					Day = 0,
-					Description = sessionData.description ,
 					Key = sessionData._id,
-					Title = sessionData.description,
-					SpeakerKey = sessionData.contributor,
-					//When = sessionData.start
-						
+					Day = DayToInt (sessionData.day),
 
-				//	Id = sessionData._id,
-				//	RoomName = sessionData.location,
-				//	Slot = new Slot(),
-				//	SlotId = String.Empty,
-				//	SpeakerTwitterName = String.Empty,
-				//	SpeakerWebsiteURL = String.Empty,
-				//	Speaker = sessionData.contributor,
-				//	Synopsis = String.Empty,
-				//	Title = sessionData.description,
-				//	TrackName = String.Empty
+					When = MakeDateTime(sessionData.day,sessionData.start),
+					Until = MakeDateTime(sessionData.day, sessionData.end),
+	
+					Description = sessionData.description,
+					SpeakerKey = sessionData.name,
+					Type = sessionData.type,
+					Level = "0",
+					Where = sessionData.location
+
 				};
 
-				Sessions.Add(session.SpeakerKey, new SessionWithFavoriteFlag { Session = session});
+				Sessions.Add (session.Key, new SessionWithFavoriteFlag { Session = session });
 				i++;
 			}
 		}
 
-		private void ParseExhibitors(SessionData[] data)
+		private void ParseExhibitors (SessionData[] data)
 		{
-			Exhibitors = new Dictionary<string, Sponsor>();
+			Exhibitors = new Dictionary<string, Sponsor> ();
 			var i = 0;
-			foreach (var location in data.Select(sessionData => sessionData.location).Where(s => !string.IsNullOrEmpty(s)).Distinct())
-			{
-				if (!Exhibitors.ContainsKey(location))
-				{
-					Exhibitors.Add(location,
-						new Sponsor
-						{
+			foreach (var location in data.Select(sessionData => sessionData.location).Where(s => !string.IsNullOrEmpty(s)).Distinct()) {
+				if (!Exhibitors.ContainsKey (location)) {
+					Exhibitors.Add (location,
+						new Sponsor {
 							Description = location,
 							//DisplayOrder = i,
 							Level = "Premium",
@@ -271,17 +297,14 @@ namespace Cirrious.Conference.Core.Models
 			}
 		}
 
-		private void ParseSponsors(SessionData[] data)
+		private void ParseSponsors (SessionData[] data)
 		{
-			Sponsors = new Dictionary<string, Sponsor>();
+			Sponsors = new Dictionary<string, Sponsor> ();
 			var i = 0;
-			foreach (var sessionData in data.Where(sessionData => !string.IsNullOrEmpty(sessionData.name)))
-			{
-				if (!Sponsors.ContainsKey(sessionData.name))
-				{
-					Sponsors.Add(sessionData.name,
-						new Sponsor
-						{
+			foreach (var sessionData in data.Where(sessionData => !string.IsNullOrEmpty(sessionData.name))) {
+				if (!Sponsors.ContainsKey (sessionData.name)) {
+					Sponsors.Add (sessionData.name,
+						new Sponsor {
 							Description = sessionData.description,
 							//DisplayOrder = i,
 							Level = "Premium",
@@ -293,5 +316,5 @@ namespace Cirrious.Conference.Core.Models
 				i++;
 			}
 		}
-    }
+	}
 }
